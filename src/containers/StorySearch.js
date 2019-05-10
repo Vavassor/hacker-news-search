5fdfdf820a04d0de302ce9de4d0153be6900bbd0
Andrew Dawson <dawso.andrew@gmail.com>
2019-05-10 15:48:23 -0400

    Starts the page not so empty by displaying the results for an empty query.

--- src/containers/StorySearch.js
+++ src/containers/StorySearch.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import {search} from "../actionCreators";
 import SearchBox from "../components/SearchBox";
 
 function StorySearch(props) {
   const {dispatch} = props;
+
+  // Load the initial page with results by searching for nothing.
+  useEffect(() => {
+    dispatch(search(""));
+  }, [dispatch]);
 
   return (
     <SearchBox handleSearch={query => dispatch(search(query))} />
